809a0ce1bb731309a51c9742a2eded15177f1930
Gaurav Kumar Maurya <gauravKmaurya09@gmail.com>
2026-01-22 16:16:19 +0530

    Time: 4 ms (21.37%), Space: 41.3 MB (39.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,14 +7,13 @@ public:
         for(int i=1; i<n; i++){
             left[i] = left[i-1]*nums[i-1];
         }
-        for(int i=n-2; i>=0; i--){
-            right[i] = right[i+1]*nums[i+1];
+
+        int prod = 1;
+        for(int i=n-1; i>=0; i--){
+            left[i] = left[i]*prod;
+            prod = prod*nums[i];
         }
 
-        vector<int> ans;
-        for(int i=0; i<n; i++){
-            ans.push_back(left[i]*right[i]);
-        }
-        return ans;
+        return left;
     }
 };

bd18b2db6f732085294c53b5c3b5bf24dc4daaaf
Gaurav Kumar Maurya <gauravKmaurya09@gmail.com>
2026-01-29 19:17:39 +0530

    Time: 0 ms (100%), Space: 8 MB (11.19%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int cnt = 0;
+        while (startValue < target) {
+            if (target % 2 == 0) {
+                target /= 2;
+            } else {
+                target += 1;
+            }
+            cnt++;
+        }
+        // cout <<(startValue - target)<< endl;
+        cnt += (startValue - target);
+        return cnt;
+    }
+};
